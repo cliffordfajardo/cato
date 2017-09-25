e26c1f62e83005e037330c731d8996ec691dd00a
Oliver <rift95@live.se>
2017-09-26 00:16:42 +0200

    Added "Shorten Url" command

---
+++ app/plugins/shorten-url/index.js
@@ -0,0 +1,47 @@
+const browser = require('webextension-polyfill');
+const $ = require('jquery');
+const plugin = {
+    keyword: "Shorten URL",
+    subtitle: 'Copies a shortened version of the current url to your clipboard',
+    action: shortenUrl,
+    icon: {
+        path: 'images/chrome-icon.svg'
+    }
+};
+
+async function shortenUrl() {
+    const tabs = await browser.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    const activeTabUrl = tabs[0].url;
+    const postUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAMSEYpKsnx7pK1I-oKYX7UMC2wumDDjk8'; // Free key allows for 1,000,000 requests per day
+
+    const shortUrl = null;
+
+    document.addEventListener('copy', (event) => {
+        event.preventDefault();
+        event.clipboardData.setData('text/plain', shortenUrl);
+    }, { once: true })
+
+    const post = $.ajax({
+        url: postUrl,
+        type: 'POST',
+        dataType: 'json',
+        contentType : 'application/json',
+        cache: true,
+        data: JSON.stringify({
+            longUrl: activeTabUrl
+        }),
+    });
+
+    post.done(data => {
+        shortenUrl = data.id;
+        document.execCommand('copy');        
+    }).fail(e => {
+        console.log('Failed to shorten url: ', activeTabUrl);
+    });
+}
+
+module.exports = plugin;
