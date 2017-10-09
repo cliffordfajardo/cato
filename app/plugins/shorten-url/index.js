721f5e1cc9c98ac61b8b895e4aa91c2ce835d04b
Oliver <rift95@live.se>
2017-10-09 11:58:43 +0200

    Added "loading/waiting" suggestion while retireving web responce + enclosed web request in try/cache clause.

--- app/plugins/shorten-url/index.js
+++ app/plugins/shorten-url/index.js
@@ -17,38 +17,50 @@ async function shortenUrl() {
   const postUrl = 'https://www.googleapis.com/urlshortener/v1/url?key=AIzaSyAMSEYpKsnx7pK1I-oKYX7UMC2wumDDjk8'; // Free key allows for 1,000,000 requests per day
   
   const request = new Request(postUrl, {
-    'method': 'POST',
-    'body': JSON.stringify({longUrl: activeTabUrl}),
-    'mode': 'cors',
-    'headers': new Headers({'Content-Type': 'application/json'})
-  })
-  
-  const data = await (await fetch(request)).json();
-  const suggestion = null;
-  if (data.id) {
-    suggestion = {
-        keyword: `${data.id}`,
-        subtitle: 'Copy shortened url to your clipboard.',
-        action: () => {
-          copyToClipboard(data.id, ev => {
-              window.close();
-          });
-        },
-        icon: {
-          path: plugin.icon.path
-        }
-      }
-  } else {
-    suggestion = {
-        keyword: "Sorry, we couldn't shorten your URL at this time.",
-        action: 'The network must be down or our API limit has exceeded.',
-        subtitle: 'Please try again later.',
-        icon: {
-          path: plugin.icon.path
-        }
-      }
+    method: 'POST',
+    body: JSON.stringify({longUrl: activeTabUrl}),
+    mode: 'cors',
+    headers: new Headers({'Content-Type': 'application/json'})
+  });
+
+  //clear suggestions display a temporary suggestion while we wait for a response
+  const temporarySuggestion = {
+    keyword: "Attempting to shorten url.",
+    subtitle: '...',
+    icon: plugin.icon
   }
-  renderSuggestions([suggestion]);
+  window.searchResultsList.innerHTML = ''
+  window.currentSearchSuggestions = [temporarySuggestion]
+  renderSuggestions(window.currentSearchSuggestions)
+  
+  let data = null;
+  let suggestion = null;
+  try {
+    data =  await (await fetch(request)).json();
+    if (!data.id) throw new Error('Property ID not found on response object');
+    suggestion = {
+      keyword: `${data.id}`,
+      subtitle: 'Copy shortened url to your clipboard.',
+      action: () => {
+        copyToClipboard(data.id, ev => {
+            window.close();
+        });
+      },
+      icon: plugin.icon
+    };
+  } catch (e) {
+    data = {
+      id: activeTabUrl
+    };
+    suggestion = {
+      keyword: "Sorry, we couldn't shorten your URL at this time.",
+      subtitle: 'Either this page is not available online, or the API limit has been exceeded.',
+      icon: plugin.icon
+    };
+  }
+  window.searchResultsList.innerHTML = ''
+  window.currentSearchSuggestions = [suggestion]
+  renderSuggestions(window.currentSearchSuggestions)
 }
 
 module.exports = plugin;
